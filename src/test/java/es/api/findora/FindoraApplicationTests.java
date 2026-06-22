3ddf71b20797abd11591b0e74c28827be719c79b
mamugo1432 <mamugo1432@gmail.com>
2026-06-22 11:36:20 +0200

    Añadiendo la estructura inicial del proyecto

---
+++ src/test/java/es/api/findora/FindoraApplicationTests.java
@@ -0,0 +1,13 @@
+package es.api.findora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
