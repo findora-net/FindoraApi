b70e8b3201b52a988dd995a740ca63786fd526a9
luismm2006 <luismulamarquez@gmail.com>
2026-06-30 11:52:17 +0200

    Subida incial de lógica e implementación de register, aún faltan mejoras por no tener persistencia finalizada, está planteado por encima de momento, a la espera de completarlo aún más.

--- src/main/java/es/api/findora/domain/model/User.java
+++ src/main/java/es/api/findora/domain/model/User.java
@@ -1,11 +1,13 @@
 package es.api.findora.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class User {
 
     private final int POINT_LIMIT = 10;
---
+++ src/main/java/es/api/findora/domain/port/in/AddUserRegisterUseCase.java
@@ -0,0 +1,9 @@
+package es.api.findora.domain.port.in;
+
+import es.api.findora.domain.model.User;
+
+public interface AddUserRegisterUseCase {
+
+    User execute(User user);
+
+}
