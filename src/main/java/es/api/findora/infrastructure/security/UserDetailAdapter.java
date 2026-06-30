e3dada962016f9b0fca894fc1ae31393fde853c5
Ivan Fernandez Mendez <ivanfernandezmen@gmail.com>
2026-06-30 14:12:09 +0200

    Configure spring security to work with JWT
    # Conflicts:
    #	pom.xml
    #	src/main/java/es/api/findora/infrastructure/persistence/entity/UserEntity.java
    #	src/main/resources/application.properties

---
+++ src/main/java/es/api/findora/infrastructure/security/UserDetailAdapter.java
@@ -0,0 +1,36 @@
+package es.api.findora.infrastructure.security;
+
+import es.api.findora.domain.model.User;
+import lombok.AllArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class UserDetailAdapter implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> autoridades = new ArrayList<>();
+
+        autoridades.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+        return autoridades;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
